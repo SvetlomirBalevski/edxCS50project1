c89a41183fdef4881c3855851e5223cda81c0d4b
SvetlomirBalevski <s.balevski@gmail.com>
2018-07-19 22:22:15 +0300

    Add templates

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template,request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,6 +21,32 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
-    return "Project 1: TODO"
+    # flights = db.execute("SELECT * FROM flights").fetchall()
+    # return render_template("index.html", flights=flights)
+    # return render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    """Please Enter your credentials."""
+
+    # # Get form information.
+    name = request.form.get("name")
+    # name = request.form.get("name")
+    # try:
+    #     flight_id = int(request.form.get("flight_id"))
+    # except ValueError:
+    #     return render_template("error.html", message="Invalid flight number.")
+
+    # # Make sure the flight exists.
+    # if db.execute("SELECT * FROM flights WHERE id = :id", {"id": flight_id}).rowcount == 0:
+    #     return render_template("error.html", message="No such flight with that id.")
+    # db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)",
+    #         {"name": name, "flight_id": flight_id})
+    # db.commit()
+    return render_template("success.html")
+@app.route("/register", methods=["GET","POST"])
+def login():
+    return render_template("login.html")
