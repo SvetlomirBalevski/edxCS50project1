491e4f5ac0554aff409da7cacfc937806807fd02
SvetlomirBalevski <s.balevski@gmail.com>
2018-08-02 17:46:31 +0300

    Housekeeping due to OS change

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
 import os
+import gc
 
-from flask import Flask, session, render_template,request
+from flask import Flask, session, render_template,request,url_for, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from wtforms import Form, BooleanField, TextField, PasswordField, validators, StringField
+from passlib.hash import sha256_crypt
+
 
 app = Flask(__name__)
 
@@ -20,33 +24,60 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    # flights = db.execute("SELECT * FROM flights").fetchall()
-    # return render_template("index.html", flights=flights)
-    # return render_template("index.html")
     return render_template("index.html")
 
 @app.route("/register", methods=["GET","POST"])
 def register():
     """Please Enter your credentials."""
 
-    # # Get form information.
-    name = request.form.get("name")
-    # name = request.form.get("name")
-    # try:
-    #     flight_id = int(request.form.get("flight_id"))
-    # except ValueError:
-    #     return render_template("error.html", message="Invalid flight number.")
+    try:
+        form = RegistrationForm(request.form)
 
-    # # Make sure the flight exists.
-    # if db.execute("SELECT * FROM flights WHERE id = :id", {"id": flight_id}).rowcount == 0:
-    #     return render_template("error.html", message="No such flight with that id.")
-    # db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)",
-    #         {"name": name, "flight_id": flight_id})
-    # db.commit()
+        if request.method == "POST" and form.validate():
+            username  = form.username.data
+            email = form.email.data
+            password = sha256_crypt.encrypt((str(form.password.data)))
+            c, conn = connection()
+
+            x = c.execute("SELECT * FROM users WHERE username = (%s)",
+                          (thwart(username)))
+
+            if int(x) > 0:
+                flash("That username is already taken, please choose another")
+                return render_template('register.html', form=form)
+
+            else:
+                c.execute("INSERT INTO users (username, password, email, tracking) VALUES (%s, %s, %s, %s)",
+                          (thwart(username), thwart(password), thwart(email), thwart("/introduction-to-python-programming/")))                
+                conn.commit()
+                flash("Thanks for registering!")
+                c.close()
+                conn.close()
+                gc.collect()
+
+                session['logged_in'] = True
+                session['username'] = username
+
+                return redirect(url_for('dashboard'))
+
+        return render_template("register.html", form=form)
+
+    except Exception as e:
+        return(str(e))
+
     return render_template("success.html")
-@app.route("/register", methods=["GET","POST"])
+@app.route("/login", methods=["GET","POST"])
 def login():
     return render_template("login.html")
